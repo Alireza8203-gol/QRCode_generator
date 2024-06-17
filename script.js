05b3f321fb88ce26d1750867183106f330685241
Alireza <alirezagolsorkhi1400@gmail.com>
2024-06-17 22:12:34 +0330

    QR Code Generator project. with pure JS.

---
+++ script.js
@@ -0,0 +1,40 @@
+const APIUrl = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=";
+const wrapper = document.querySelector(".wrapper");
+const textInput = document.querySelector("#text");
+const qrCodeImage = document.querySelector("img");
+const requestBtn = document.querySelector("button");
+const qrCodeImageURL = document.querySelector("#qr_code_URL");
+const qrCodeImageSize = document.querySelector("#form_select");
+
+function createQRCode() {
+  let inputValue = textInput.value.trim();
+  let size = qrCodeImageSize.value;
+
+  if (inputValue) {
+    qrCodeImage.setAttribute("src", `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${inputValue}`);
+    requestBtn.innerHTML = "Generating QR Code ...";
+
+    qrCodeImage.addEventListener("load", () => {
+
+      wrapper.classList.add("active");
+
+      requestBtn.innerHTML = "Generate QR Code";
+
+      qrCodeImageURL.setAttribute("value", `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${inputValue}`);
+
+      qrCodeImageURL.addEventListener("click", (e) => {
+        e.target.select();
+        document.execCommand('copy');
+        alert("Copied Successfully!")
+      });
+    })
+  }
+}
+
+textInput.addEventListener("keyup", (e) => {
+  if (!textInput.value.trim()) {
+    wrapper.classList.remove("active");
+  }
+})
+
+requestBtn.addEventListener("click", createQRCode);
